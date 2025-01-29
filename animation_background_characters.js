40b2049ac8789c842f5825e7fd46f3c9d941897e
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-29 18:29:12 +0100

    App: 'Animation Background Characters' ! create fn fontSize()  !

--- animation_background_characters.js
+++ animation_background_characters.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
-ctx.font = "bold 16px monospace";
-ctx.fillStyle = 'blue';
+const fontSize = () => 16;
 
-ctx.fillText('Hello World!', width / 2, height / 2);
+ctx.font = `bold ${fontSize()}px monospace`;
+ctx.fillStyle = "blue";
+
+ctx.fillText("Hello World!", width / 2, height / 2);
+ctx.fillText("Hello World!", width / 2, height / 2 + 16);
