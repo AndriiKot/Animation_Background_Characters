1a47dc500ade174f95723a83ed6f59ba36788f3d
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-29 18:33:33 +0100

    App: 'Animation Background Characters' ! create fn animation() testing 1 up-to-down letters  !

--- animation_background_characters.js
+++ animation_background_characters.js
@@ -7,9 +7,18 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 const fontSize = () => 16;
+let y =  0;
 
 ctx.font = `bold ${fontSize()}px monospace`;
 ctx.fillStyle = "blue";
 
-ctx.fillText("Hello World!", width / 2, height / 2);
-ctx.fillText("Hello World!", width / 2, height / 2 + 16);
+ctx.fillText("H", 50, y);
+
+const animation = () => {
+    requestAnimationFrame( animation );
+    y += 1;
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillText("H", 50, y);
+};
+
+animation();
