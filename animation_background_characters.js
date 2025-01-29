6d04faab33dcd04e36c83322838aba66af32b149
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-29 18:23:20 +0100

    App: 'Animation Background Characters' ! create fistr element 'Hello World!' testing  !

--- animation_background_characters.js
+++ animation_background_characters.js
@@ -5,3 +5,8 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+ctx.font = "bold 16px monospace";
+ctx.fillStyle = 'blue';
+
+ctx.fillText('Hello World!', width / 2, height / 2);
